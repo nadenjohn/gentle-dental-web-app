158c9d7b85983a234f36a832b044ee0047ace6e5
John <nadenjohn@gmail.com>
2022-07-18 17:35:11 -0700

    clear it up

--- client/src/components/Dentists.js
+++ client/src/components/Dentists.js
@@ -4,9 +4,9 @@ function Dentists({dentists, user, handlePost}) {
     const dentistItems = dentists.map((dentist) => (
       <DentistItem 
         key={dentist.id}
-          dentist={dentist}
-          user={user}
-          handlePost={handlePost}
+        dentist={dentist}
+        user={user}
+        handlePost={handlePost}
       />
     ))
   return (
